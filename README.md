da32f99366e0423ee966e9bcb1e20fe483e07a20
LEO-Malay <malaybhavsar.290@gmail.com>
2022-09-23 23:52:28 +0530

    Version 2.0

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### Easy & Simple implementation of blockchian
 
-Hello World here is my blockchain implementation which you can use anywhere without more effort weather t be a simple wallet project or a transaction project. I have made use of Crypto Library of NodeJS to generate "SHA256" for the blocks.
+Hello World here is my blockchain implementation which you can use anywhere without more effort whether it be a simple wallet project or a transaction project. I have made use of Crypto Library of NodeJS to generate "SHA256" for the blocks.
 
 ```jsx
 // Import the module
@@ -12,6 +12,36 @@ const blockchain = require("leo-blockchain");
 <br/>
 
 Following are the functionality provided by the module.
+
+<b>(1) Perform Transaction</b>
+
+```jsx
+blockchain.addTransaction(data); // datatype of "data" ---> JSON
+```
+
+<b>(2) Search Transaction by transactionId</b>
+
+```jsx
+blockchain.searchTransaction(transactionId); // datatype of "transactionId" ---> Number
+```
+
+<b>(3) Set Difficulty</b>
+
+```jsx
+blockchain.addTransaction(difficulty); // datatype of "difficulty" ---> Number
+```
+
+<b>(4) Export your Chain</b>
+
+```jsx
+blockchain.exportBlocks("fileName.xyz");
+```
+
+<b>(5) Import your Chain</b>
+
+```jsx
+blockchain.importBlocks("fileName.xyz");
+```
 
 <b>(1) Add a new Block to the chain</b>
 
--- index.js
+++ index.js
@@ -1,148 +1,261 @@
-/**
- * Developed with love by Malay Bhavsar <malaybhavsar.290@gmail.com>
- */
 const { SHA256 } = require("crypto-js");
 const { writeFileSync, readFileSync } = require("fs");
-class Block {
-    constructor(blockId, timestamp, data, prevHash) {
+
+class Transaction {
+    constructor(blockId, transactionId, datetime, data, prevHash) {
         this.blockId = blockId;
-        this.timestamp = timestamp;
+        this.transactionId = transactionId;
+        this.datetime = datetime;
+        this.data = data;
         this.prevHash = prevHash;
-        this.data = JSON.stringify(data);
-        this.dataHash = SHA256(this.data).toString();
-        this.nonce = 0;
-        this.hash = this.calculateHash();
+        this.hash = this.calcHash();
     }
-    calculateHash() {
+    calcHash() {
         return SHA256(
-            [
-                this.blockId,
-                this.timestamp,
-                this.prevHash,
-                this.data,
-                this.dataHash,
-                this.nonce,
-            ].join("@-#_$")
+            JSON.stringify({
+                blockId: this.blockId,
+                transactionId: this.transactionId,
+                datetime: this.datetime,
+                data: this.data,
+                prevHash: this.prevHash,
+            })
         ).toString();
     }
-    mineBlock(key = 2) {
-        while (this.hash.substring(0, key) !== Array(key + 1).join("0")) {
-            this.nonce++;
-            this.hash = this.calculateHash();
-        }
-        console.log(
-            "[+] Block Mined -> BlockId:",
-            this.blockId + "\tNonce:",
-            this.nonce
-        );
+    toJSON() {
         return {
-            blockId: this.blockId,
-            timestamp: this.timestamp,
+            transactionId: this.transactionId,
+            datetime: this.datetime,
+            data: this.data,
             prevHash: this.prevHash,
-            data: JSON.parse(this.data),
             hash: this.hash,
         };
     }
-    verifyBlock(key = 2) {
-        while (this.hash.substring(0, key) !== Array(key + 1).join("0")) {
+}
+class Block {
+    constructor(blockId, prevHash, difficulty) {
+        this.block = [];
+        this.blockId = blockId;
+        this.transactionId = this.blockId * 8;
+        this.transactionCount = 0;
+        this.nonce = 0;
+        this.prevTransactionHash = undefined;
+        this.difficulty = difficulty;
+        this.prevHash = prevHash;
+        this.datetime = undefined;
+        this.foundationHash = undefined;
+        this.hash = undefined;
+    }
+    addTransaction(datetime, data, prevHash) {
+        if (this.transactionCount < 8) {
+            let t = new Transaction(
+                this.blockId,
+                this.transactionId + this.transactionCount,
+                datetime,
+                data,
+                prevHash
+            );
+            this.prevTransactionHash = t.hash;
+            this.block.push(t.toJSON());
+            this.transactionCount++;
+            return { success: true, transactionId: t.transactionId };
+        } else return { success: false, transactionId: undefined };
+    }
+    calcFoundationHash() {
+        function hash(hdata) {
+            if (hdata.length === 0) return;
+            if (hdata.length === 1) return hdata[0];
+            let temp = [];
+            for (let i = 0; i < hdata.length / 2; i += 2)
+                temp.push(SHA256(hdata[i] + hdata[i + 1]).toString());
+            return hash(temp);
+        }
+        let dataHash = [];
+        for (let a = 0; a < this.transactionCount; a++)
+            dataHash.push(SHA256(JSON.stringify(this.block[a])).toString());
+
+        return hash(dataHash);
+    }
+    calculateHash() {
+        return SHA256(
+            JSON.stringify({
+                blockId: this.blockId,
+                data: this.block,
+                foundationHash: this.foundationHash,
+                datetime: this.datetime,
+                difficulty: this.difficulty,
+                prevHash: this.prevHash,
+                hash: this.hash,
+            })
+        ).toString();
+    }
+    mineBlock(datetime) {
+        if (this.transactionCount !== 8) return { success: false };
+        this.datetime = datetime;
+        this.foundationHash = this.calcFoundationHash();
+        this.hash = this.calculateHash();
+        while (
+            this.hash.substring(0, this.difficulty) !==
+            Array(this.difficulty + 1).join("0")
+        ) {
             this.nonce++;
             this.hash = this.calculateHash();
         }
-        return this.hash;
+        return { success: true };
+    }
+    toJSON() {
+        return {
+            blockId: this.blockId,
+            data: this.block,
+            foundationHash: this.foundationHash,
+            datetime: this.datetime,
+            difficulty: this.difficulty,
+            prevHash: this.prevHash,
+            hash: this.hash,
+            nonce: this.nonce,
+        };
     }
 }
-class BlockChain {
-    constructor(difficulty = 2) {
-        this.difficulty = difficulty;
-        this.chain = [this.genesisBlock()];
+class Blockchain {
+    constructor() {
+        this.difficulty = 3;
+        this.prevTransactionHash = undefined;
+        this.prevBlockHash = undefined;
+        this.chain = this.genesisBlock();
         this.blockId = 0;
+        this.blockHold = new Block(this.blockId, "0", this.difficulty);
     }
     genesisBlock() {
-        let block = new Block(0, "01/01/2022 00:00:00", {}, "0");
-        return block.mineBlock(this.difficulty);
-    }
-    getLatestBlockHash() {
-        if (this.blockId === 0) return "0";
-        return this.chain[this.blockId - 1].hash;
-    }
-    genTimeStamp() {
-        let date = new Date();
-        return [
-            [date.getDate() + 1, date.getMonth() + 1, date.getFullYear()].join(
-                "/"
-            ),
-            [
-                date.getHours(),
-                date.getMinutes(),
-                date.getSeconds(),
-                date.getMilliseconds(),
-            ].join(":"),
-        ].join(" ");
-    }
-    addBlock(data) {
-        this.blockId++;
-        let block = new Block(
-            this.blockId,
-            this.genTimeStamp(),
-            data,
-            this.getLatestBlockHash()
-        );
-        this.chain.push(block.mineBlock(this.difficulty));
-        return this.chain[this.blockId];
-    }
-    setDifficulty(difficulty) {
-        console.log("[+] Difficulty set to", this.difficulty);
-        this.difficulty = difficulty;
-        this.chain = [this.genesisBlock()];
-        this.blockId = 0;
-    }
-    /** Additional Function */
-    importBlocks(fileName) {
-        this.chain = this.chain.concat(
-            JSON.parse(readFileSync(fileName + ".txt", "utf-8"))
-        );
-        if (this.verifyChain()) {
-            console.log(
-                "[+] Data Imported. Total Number of Blocks:",
-                this.chain.length
+        let temp = new Block(-1, "0", this.difficulty);
+        for (let i = 0; i < 8; i++) {
+            temp.addTransaction(
+                "01/01/2022 00:00:00:000",
+                {},
+                this.prevTransactionHash
             );
+            this.prevTransactionHash = temp.prevTransactionHash;
+        }
+        temp.mineBlock("01/01/2022 00:00:00:000");
+        temp = temp.toJSON();
+        this.prevBlockHash = temp.hash;
+        return [temp];
+    }
+    addTransaction(data, datetimestamp = undefined) {
+        /**
+         * @param {data} JSON Pass JSON data to be stored
+         *
+         * @returns {success: Boolean, transactionId: Number};
+         */
+        function genTimeStamp() {
+            let date = new Date();
+            return [
+                [
+                    date.getDate() + 1,
+                    date.getMonth() + 1,
+                    date.getFullYear(),
+                ].join("/"),
+                [
+                    date.getHours(),
+                    date.getMinutes(),
+                    date.getSeconds(),
+                    date.getMilliseconds(),
+                ].join(":"),
+            ].join(" ");
+        }
+        datetimestamp =
+            datetimestamp !== undefined ? datetimestamp : genTimeStamp();
+        let t = this.blockHold.addTransaction(
+            datetimestamp,
+            data,
+            this.prevTransactionHash
+        );
+        if (t.success === true) {
+            this.prevTransactionHash = this.blockHold.prevTransactionHash;
+            return { success: true, transactionId: t.transactionId };
         } else {
-            this.chain = [this.genesisBlock()];
-            console.log("[-] Chain is compromised");
+            this.blockHold.mineBlock(datetimestamp);
+            this.blockHold = this.blockHold.toJSON();
+            this.prevBlockHash = this.blockHold.hash;
+            this.chain.push(this.blockHold);
+            this.blockId++;
+            this.blockHold = new Block(
+                this.blockId,
+                this.prevBlockHash,
+                this.difficulty
+            );
+            this.blockHold.addTransaction(
+                datetimestamp,
+                data,
+                this.prevTransactionHash
+            );
+            this.prevTransactionHash = this.blockHold.prevTransactionHash;
+            return { success: true, transactionId: t.transactionId };
+        }
+    }
+    searchTransaction(transactionId) {
+        /**
+         * @param {transactionId} Number Unique transactionId recieved while adding transaction
+         *
+         * @return {success: Boolean, data: Object}
+         */
+        let blockId = Math.floor(transactionId / 8);
+        let transactionCountId = transactionId % 8;
+        try {
+            return {
+                success: true,
+                data: this.chain[blockId + 1].data[transactionCountId],
+            };
+        } catch {
+            console.log("[ERROR]: Please check your transaction number!");
+            return { success: false, data: null };
         }
     }
     exportBlocks(fileName) {
-        writeFileSync(fileName + ".txt", JSON.stringify(this.chain.slice(1)));
+        /**
+         * @param {fileName} String Filename with extension
+         *
+         * @return {success: Boolean}
+         */
+        writeFileSync(
+            fileName,
+            JSON.stringify({
+                chain: this.chain.slice(1),
+                unminedBlock: this.blockHold.toJSON(),
+            })
+        );
+        return { success: true };
     }
-    /** Verification Function */
-    verifyChain() {
-        let blockId = 1;
-        while (blockId < this.blockId) {
-            if (this.chain[blockId].prevHash !== this.chain[blockId - 1].hash) {
-                return false;
+    importBlocks(fileName) {
+        /**
+         * @param {fileName} String Filename with extension
+         *
+         * @return {success: Boolean}
+         */
+        try {
+            let data = JSON.parse(readFileSync(fileName, "utf-8"));
+            for (let i = 0; i < data.chain.length; i++) {
+                for (let j = 0; j < data.chain[i].data.length; j++)
+                    this.addTransaction(
+                        data.chain[i].data[j].data,
+                        data.chain[i].data[j].datetime
+                    );
             }
-            blockId++;
+            for (let k = 0; k < data.unminedBlock.data.length; k++)
+                this.addTransaction(
+                    data.unminedBlock.data[k].data,
+                    data.unminedBlock.data[k].datetime
+                );
+            return { success: true };
+        } catch {
+            console.error("[ERROR]: No such file found!");
+            return { success: false };
         }
-        return true;
     }
-    verifyBlock(blockId) {
-        if (blockId < this.blockId) {
-            let block = new Block(
-                blockId,
-                this.chain[blockId].timestamp,
-                this.chain[blockId].data,
-                this.chain[blockId].prevHash
-            );
-            if (
-                block.verifyBlock(this.difficulty) ===
-                this.chain[blockId + 1].prevHash
-            )
-                return true;
-            else return false;
-        } else {
-            console.log("[-] Unable to verify latest block");
-            return false;
-        }
+    setDifficulty(difficulty) {
+        /**
+         * @param {difficulty} Number Set difficulty
+         */
+        this.difficulty = difficulty;
     }
 }
-module.exports = new BlockChain();
+
+module.exports = new Blockchain();
